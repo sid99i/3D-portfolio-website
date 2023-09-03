b9d4fd4d29a4e17f0e5a1cd83b99f4b1b6f3bfc0
sidd9i <siddharthb324@gmail.com>
2023-09-03 08:32:07 +0530

    updating about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -40,18 +40,18 @@ const About = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled software developer with experience in python, C++ 
+        and JavaScript, and expertise in frameworks like React, Node.js,
+        and Three.js. I'm a quick learner and collaborate closely to create 
+        efficient, scalable, and user-friendly solutions that solve real 
+        world problems.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
